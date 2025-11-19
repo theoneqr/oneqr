626a4626ef194a232b68f1d458c2c3e841f1fb7d
Dustin Nguyen <avsecuri@gmail.com>
2025-11-19 18:11:18 +0700

    upload

---
+++ dist/index.d.ts
@@ -0,0 +1,7 @@
+declare const oneqr: {
+    hello(): string;
+};
+export type Oneqr = typeof oneqr;
+export { oneqr };
+export default oneqr;
+//# sourceMappingURL=index.d.ts.map
---
+++ dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.oneqr = void 0;
+const oneqr = {
+    hello() {
+        return "hello, world";
+    },
+};
+exports.oneqr = oneqr;
+exports.default = oneqr;
+//# sourceMappingURL=index.js.map
